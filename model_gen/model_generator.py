c5ae756a96bf06290b14fcc48ad9069e40cd51fc
ChrisO <39662993+ChrisOh431@users.noreply.github.com>
2021-12-10 23:58:48 -0600

    model gen changes

--- model_gen/model_generator.py
+++ model_gen/model_generator.py
@@ -4,13 +4,13 @@ from sklearn import datasets
 from random import randint
 
 
-def create_reguession_dataset(noise, count):
+def create_reguession_dataset(noise, count, randvar):
     x, y, coef = datasets.make_regression(n_samples=count,  # number of samples
                                           n_features=1,  # number of features
                                           n_informative=1,  # number of useful features
                                           noise=noise,  # bias and standard deviation of the guassian noise
                                           coef=True,  # true coefficient used to generated the data
-                                          random_state=5)  # set for same data points for each run
+                                          random_state=randvar)  # set for same data points for each run
     x = np.interp(x, (x.min(), x.max()), (0, 100))
     y = np.interp(y, (y.min(), y.max()), (0, 100))
 
@@ -24,19 +24,24 @@ def create_reguession_dataset(noise, count):
     return (coef.item(), x_coords, y_coords)
 
 
-def serialize_create_reguession_dataset(dataset, location=""):
-    jsondata = {
-        "x_vals": dataset[1],
-        "y_vals": dataset[2],
-        "coeff": dataset[0],
-        "y_int": 0
-    };
+def serialize_reguession_datasets(location=""):
+    sets = [0, 1, 2, 3, 4, 5]
 
     if location:
-        with open("location", "w") as datasetfile:
-            json.dump(jsondata, datasetfile, indent=4)
+        with open(location, "w") as datasetfile:
+            for randset in sets:
+                dataset = create_reguession_dataset(5, 10, randset)
 
+                jsondata = {
+                    "x_vals": dataset[1],
+                    "y_vals": dataset[2],
+                    "coeff": dataset[0],
+                    "y_int": 0
+                }
+
+                json.dump(jsondata, datasetfile, indent=4)
+                
     return jsondata
 
 
-print(create_reguession_dataset(5, 5))
+serialize_reguession_datasets('./src/reguessiondatasets.json')
