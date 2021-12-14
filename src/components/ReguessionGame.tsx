d8bd8a72c595ab158a29484dbe29dc234355eb49
ChrisO <39662993+ChrisOh431@users.noreply.github.com>
2021-12-13 23:59:06 -0600

    data deserialization

--- src/components/ReguessionGame.tsx
+++ src/components/ReguessionGame.tsx
@@ -5,12 +5,32 @@ import ReguessionChartContainer from "./RegressionChartContainer";
 import { styled } from "@mui/material/styles";
 import { Box, Stack } from "@mui/material";
 
+import regression_datasets from "../reguessiondatasets.json";
+
+import { RegressionDataset } from "../scripts/regressiondata" 
+
 const TallStack = styled(Stack)(({ theme }) => ({
 	height: "100%",
 	backgroundColor: theme.palette.background.default,
 }));
 
 export default function ReguessionGame() {
+	let sets: RegressionDataset[] = [];
+
+	for (let dataset in regression_datasets)
+	{
+		let deserializeddata: RegressionDataset = {
+			x_vals: [],
+			y_vals: [],
+			coeff: 0,
+			y_int: 0
+		};
+
+		Object.assign(deserializeddata, dataset);
+		console.log(deserializeddata);
+		sets.push(deserializeddata);
+	}
+
 	<TallStack
 		direction={{ xs: "column", md: "row" }}
 		alignContent="flex-start"
@@ -22,9 +42,7 @@ export default function ReguessionGame() {
 			alignItems={"center"}
 			padding={{ xs: "0.5em", md: "5.8%" }}
 			width={{ xs: "100%", md: "60%" }}
-		>
-			<ReguessionChartContainer />
-		</Box>
+		/>
 		<Stack
 			justifyContent={{ xs: "flex-start", md: "flex-end" }}
 			alignItems={"center"}
