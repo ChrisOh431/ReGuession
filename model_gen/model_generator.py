d6b7c15b5b927e943a61121339f8c3c2b5a793c2
ChrisO <39662993+ChrisOh431@users.noreply.github.com>
2021-12-15 14:29:07 -0600

    dataset generation process changes

--- model_gen/model_generator.py
+++ model_gen/model_generator.py
@@ -5,7 +5,7 @@ from random import randint
 
 
 def create_reguession_dataset(noise, count, randvar):
-    x, y, coef = datasets.make_regression(n_samples=count,  # number of samples
+    x, y, coef = datasets.make_regression(n_samples=count+2,  # number of samples
                                           n_features=1,  # number of features
                                           n_informative=1,  # number of useful features
                                           noise=noise,  # bias and standard deviation of the guassian noise
@@ -13,6 +13,19 @@ def create_reguession_dataset(noise, count, randvar):
                                           random_state=randvar)  # set for same data points for each run
     x = [x_coord[0] for x_coord in np.interp(x, (x.min(), x.max()), (0, 100))]
     y = [y_coord for y_coord in np.interp(y, (y.min(), y.max()), (0, 100))]
+
+    # every regression included 0.0 and 100.0
+    y_min = min(y)
+    y_max = max(y)
+
+    x_of_y_min = y.index(y_min)
+    x_of_y_max = y.index(y_max)
+    
+    x.pop(x_of_y_min)
+    x.pop(x_of_y_max)
+
+    y.remove(y_min)
+    y.remove(y_max)
 
     return (coef.item(), 0, x, y)
 
